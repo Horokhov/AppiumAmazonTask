8cacd7a9cc54c2ed8d1f48469b2b06d7c254743c
Horokhov <max_horokhov@tutanota.com>
2023-08-21 12:40:09 +0300

    1

--- src/main/java/base/BaseTestAndroid.java
+++ src/main/java/base/BaseTestAndroid.java
@@ -3,8 +3,10 @@ package base;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -26,33 +28,48 @@ public class BaseTestAndroid {
 
     @BeforeMethod(alwaysRun = true)
     public void setUpAndroid() throws IOException {
-        Properties properties = new Properties();
-        FileInputStream fileInputStream = new FileInputStream("/Users/max_horokhov/Desktop/Projects/Appium/AppiumAmazonTask/src/main/java/resources/global.properties");
-        properties.load(fileInputStream);
+//        Properties properties = new Properties();
+//        FileInputStream fileInputStream = new FileInputStream("/Users/max_horokhov/Desktop/Projects/Appium/AppiumAmazonTask/src/main/java/resources/global.properties");
+//        properties.load(fileInputStream);
+//
+//        String ip = properties.getProperty("ip");
+//        String portString = properties.getProperty("portAndroid");
+//        int port = Integer.parseInt(portString);
+//        String pathToMainJs = properties.getProperty("pathToMainJs");
+//        String pathToChromedriverExecutable = properties.getProperty("pathToChromedriverExecutable");
+//        String browser = properties.getProperty("browserAndroid");
+//        String platformVersion = properties.getProperty("androidPlatformVersion");
+//        String androidDeviceName = properties.getProperty("androidDeviceName");
+//        String androidDriverMachineUrl = properties.getProperty("androidDriverMachineUrl");
+//
+//        service = new AppiumServiceBuilder().withAppiumJS(new File(pathToMainJs)).withIPAddress(ip).usingPort(port).build();
+//        service.start();
+//
+//        UiAutomator2Options options = new UiAutomator2Options();
+//        options.setDeviceName(androidDeviceName);
+//        options.setChromedriverExecutable(pathToChromedriverExecutable);
+//        options.setCapability("browserName", browser);
+//        options.setCapability("platformVersion", platformVersion);
+//        options.setCapability("newCommandTimeout", 300);
+//
+//        driver = new AndroidDriver(new URL(androidDriverMachineUrl), options);
+//        Set<String> context = driver.getContextHandles();
+//        driver.context("CHROMIUM");
+        MutableCapabilities caps = new MutableCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+        caps.setCapability("appium:deviceOrientation", "portrait");
+        caps.setCapability("appium:platformVersion", "current_major");
+        caps.setCapability("appium:automationName", "UiAutomator2");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("appiumVersion", "2.0.0");
+        sauceOptions.setCapability("username", "oauth-max_horokhov-859e7");
+        sauceOptions.setCapability("accessKey", "42a8932b-f474-495b-bcdf-7c6d8c421d84");
+        caps.setCapability("sauce:options", sauceOptions);
 
-        String ip = properties.getProperty("ip");
-        String portString = properties.getProperty("portAndroid");
-        int port = Integer.parseInt(portString);
-        String pathToMainJs = properties.getProperty("pathToMainJs");
-        String pathToChromedriverExecutable = properties.getProperty("pathToChromedriverExecutable");
-        String browser = properties.getProperty("browserAndroid");
-        String platformVersion = properties.getProperty("androidPlatformVersion");
-        String androidDeviceName = properties.getProperty("androidDeviceName");
-        String androidDriverMachineUrl = properties.getProperty("androidDriverMachineUrl");
-
-        service = new AppiumServiceBuilder().withAppiumJS(new File(pathToMainJs)).withIPAddress(ip).usingPort(port).build();
-        service.start();
-
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName(androidDeviceName);
-        options.setChromedriverExecutable(pathToChromedriverExecutable);
-        options.setCapability("browserName", browser);
-        options.setCapability("platformVersion", platformVersion);
-        options.setCapability("newCommandTimeout", 300);
-
-        driver = new AndroidDriver(new URL(androidDriverMachineUrl), options);
-        Set<String> context = driver.getContextHandles();
-        driver.context("CHROMIUM");
+        URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        driver = new AndroidDriver(url, caps);
     }
 
     @AfterMethod(alwaysRun = true)
--- src/main/java/base/IosBaseTest.java
+++ src/main/java/base/IosBaseTest.java
@@ -6,6 +6,7 @@ import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.MutableCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.AmazonHomePage;
@@ -24,29 +25,41 @@ public class IosBaseTest {
 
     @BeforeMethod(alwaysRun = true)
     public void setUpIos() throws IOException {
-        Properties properties = new Properties();
-        FileInputStream fileInputStream = new FileInputStream("/Users/max_horokhov/Desktop/Projects/Appium/AppiumAmazonTask/src/main/java/resources/global.properties");
-        properties.load(fileInputStream);
+//        Properties properties = new Properties();
+//        FileInputStream fileInputStream = new FileInputStream("/Users/max_horokhov/Desktop/Projects/Appium/AppiumAmazonTask/src/main/java/resources/global.properties");
+//        properties.load(fileInputStream);
+//
+//        String ip = properties.getProperty("ip");
+//        String portString = properties.getProperty("port");
+//        String deviceName = properties.getProperty("appleDeviceName");
+//        String browserName = properties.getProperty("browserApple");
+//        String applePlatformVersion = properties.getProperty("applePlatformVersion");
+//        int port = Integer.parseInt(portString);
+//        String pathToMainJs = properties.getProperty("pathToMainJs");
+//        String androidDriverMachineUrl = properties.getProperty("androidDriverMachineUrl");
 
-        String ip = properties.getProperty("ip");
-        String portString = properties.getProperty("port");
-        String deviceName = properties.getProperty("appleDeviceName");
-        String browserName = properties.getProperty("browserApple");
-        String applePlatformVersion = properties.getProperty("applePlatformVersion");
-        int port = Integer.parseInt(portString);
-        String pathToMainJs = properties.getProperty("pathToMainJs");
-        String androidDriverMachineUrl = properties.getProperty("androidDriverMachineUrl");
+//        service = new AppiumServiceBuilder().withAppiumJS(new File(pathToMainJs)).withIPAddress(ip).usingPort(port).build();
+//        service.start();
 
-        service = new AppiumServiceBuilder().withAppiumJS(new File(pathToMainJs)).withIPAddress(ip).usingPort(port).build();
-        service.start();
+        MutableCapabilities caps = new MutableCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("browserName", "Safari");
+        caps.setCapability("appium:deviceName", "iPhone Simulator");
+        caps.setCapability("appium:deviceOrientation", "portrait");
+        caps.setCapability("appium:platformVersion", "16.2");
+        caps.setCapability("appium:automationName", "XCUITest");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("appiumVersion", "2.0.0");
+        sauceOptions.setCapability("username", "oauth-max_horokhov-859e7");
+        sauceOptions.setCapability("accessKey", "42a8932b-f474-495b-bcdf-7c6d8c421d84");
+        caps.setCapability("sauce:options", sauceOptions);
 
-        XCUITestOptions options = new XCUITestOptions();
-        options.setDeviceName(deviceName);
-        options.setCapability("browserName", browserName);
-        options.setCapability("platformVersion", applePlatformVersion);
-        options.setCapability("newCommandTimeout", 300);
-
-        driver = new IOSDriver(new URL(androidDriverMachineUrl), options);
+        URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        driver = new IOSDriver(url, caps);
+//        options.setDeviceName(deviceName);
+//        options.setCapability("browserName", browserName);
+//        options.setCapability("platformVersion", applePlatformVersion);
+//        options.setCapability("newCommandTimeout", 300);
     }
 
     @AfterMethod(alwaysRun = true)
